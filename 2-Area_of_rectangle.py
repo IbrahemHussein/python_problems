987279258bb684e6b415a49e1b7fc4539a5f1b87
@Ibrahem Hussein <arbrahymalhsyny05@gmail.com>
2023-06-17 19:02:04 +0300

    new problem

---
+++ 2-Area_of_rectangle.py
@@ -0,0 +1,7 @@
+# TODO: ask user for input width and height
+width=float(input('enter the width: '))
+height=float(input('enter the height:'))
+
+# TODO: calculate the area of the rectangle
+area=width*height
+print('the area of the rectangle is :',area)
