8adeeecb0f46e1f09aa05cdac47f4e87682632b9
@Ibrahem Hussein <arbrahymalhsyny05@gmail.com>
2023-06-17 18:39:04 +0300

    rename file

---
+++ udemy problem/1-square number.py
@@ -0,0 +1,3 @@
+print('number'+' '*2+'square'+' '*3+'cube')
+for i in range(1,6):
+    print(str(i)+' '*8+str(i**2)+' '*7+str(i**3))
--- udemy problem/square number.py
+++
@@ -1,3 +0,0 @@
-print('number'+' '*2+'square'+' '*3+'cube')
-for i in range(1,6):
-    print(str(i)+' '*8+str(i**2)+' '*7+str(i**3))
